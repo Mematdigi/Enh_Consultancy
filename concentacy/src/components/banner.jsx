63000ac9b4c5f9b7921553216df51414f8333a97
Vikas <ordersmego@gmail.com>
2026-03-26 13:13:41 +0530

    main || add animation on the website

--- concentacy/src/components/banner.jsx
+++ concentacy/src/components/banner.jsx
@@ -5,7 +5,7 @@ function Banner() {
     <section className="hero-banner">
       <Container>
         <Row className="align-items-center">
-          <Col lg={6} className="banner-text">
+          <Col lg={6} className="banner-text mb-4">
             <h2>
               <span className="italic-text">Smarter Strategies for a Stronger Future</span>
              
@@ -22,10 +22,10 @@ function Banner() {
 
           <Col lg={6} className="banner-images">
             <div className="image-grid">
-              <img src="./service1.jpg" className="img-one tilt-left" alt="Fintech Image 1" />
-              <img src="./service2.jpg" className="img-two tilt-right" alt="Fintech Image 2" />
-              <img src="./service3.jpg" className="img-three tilt-left" alt="Fintech Image 3" />
-              <img src="./service4.jpg" className="img-four tilt-right" alt="Fintech Image 4" />
+              <img src="./service1.jpg" className="img-one" alt="Fintech Image 1" />
+              <img src="./service2.jpg" className="img-two" alt="Fintech Image 2" />
+              <img src="./service3.jpg" className="img-three" alt="Fintech Image 3" />
+              <img src="./service4.jpg" className="img-four" alt="Fintech Image 4" />
             </div>
           </Col>
         </Row>
